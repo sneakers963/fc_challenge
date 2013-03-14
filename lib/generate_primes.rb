7c1d1f76f9b5064031c72e6c8e3d60364e652866
Richard Simmonds <rich.simmonds1@gmail.com>
2013-03-14 19:24:33 +0000

    added sieve method for generating primes

--- lib/generate_primes.rb
+++ lib/generate_primes.rb
@@ -6,21 +6,51 @@ class GeneratePrimes
 		@n_primes = num
 	end
 
+	# # Basic method - not efficient as tests every odd number
+	# def primes
+	# 	primes = []
+	# 	x = 2
+	# 	loop do
+	# 	  if ((x % 2 != 0) || (x == 2)) # Only check odd numbers, and 2
+	# 	    y = 2
+	# 	    # Loop to test if x is divisible by any number below it, y
+	# 	    y += 1 until x % y == 0
+	# 	    # If the number was not divisible by any below itself, it is a prime
+	# 	    primes << x if x == y
+	# 	    # End loop when desired number has been calculated
+	# 	    break if primes.length == @n_primes
+	# 	  end
+	# 	  x += 1
+	# 	end
+	# 	return primes
+	# end
+
+	# Uses O'Niell's method of implementing the Sieve of Eratosthenes to produce
+	# first n primes. See http://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf
 	def primes
 		primes = []
-		# This is not very efficient as it tests every odd number...?
-		x = 2
+		@priority_queue = []
+
+		def check_pq(x)
+			@priority_queue.include? x
+		end
+		
+		def pq_index(x)
+			@priority_queue.index(x)
+		end
+
+		primes << 2
+		x = 3
+		
 		loop do
-		  if ((x % 2 != 0) || (x == 2)) # Only check odd numbers, and 2
-		    y = 2
-		    # Loop to test if x is divisible by any number below it, y
-		    y += 1 until x % y == 0
-		    # If the number was not divisible by any below itself, it is a prime
-		    primes << x if x == y
-		    # End loop when desired number has been calculated
-		    break if primes.length == @n_primes
-		  end
-		  x += 1
+			break if primes.length == @n_primes
+			if check_pq(x)
+				@priority_queue[pq_index(x)] = x + @priority_queue[pq_index(x)+1] while check_pq(x)
+			else
+				primes << x			
+			end
+			@priority_queue << x**2 << 2*x
+			x += 2
 		end
 		return primes
 	end
